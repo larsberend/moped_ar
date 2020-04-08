da546839f6f4d93be0374ed62341c0a988192358
larsberend <berend.brandt@gmail.com>
2020-04-08 16:41:40 +0200

    Draw 3D curve, transform to camera frame

---
+++ draw_curve.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sympy import Point3D, Circle
+from skimage.draw import circle_perimeter
+# kreis in koordinatensystem. ursprung: schnittpunkt motorrad, ebene (stuetzpunkt)
+# einheit: cm
+# kreis liegt auf ebene, also Y-Koordinate konstant 0
+
+# zu bild Koordinaten:
+# bildX = f * ()
+
+# actual focal length = equivalent focal length / crop factor
+# 19/5.6 = 
+
+class Curve():
+    def __init__(self, radius):
+        self.c = Circle(Point3D([radius, 0, 0]), radius)
+
+
+if __name__=='__main__':
+    radius = 5
+    curve = Curve(radius)
+    print(curve.c)
+
+    curve2 = circle_perimeter(np.int(radius*10), 0, np.abs(radius))
+    curve3d = curve2 + (np.zeros(curve2[0].shape, dtype=np.int64), )
+    print('Kurve in world coords')
+    print(curve3d)
+
+    curve3d= np.column_stack(curve3d)
+    print(curve3d[0])
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 from cordFrames import cordFrame, worldFrame, transform
 from scipy.spatial.transform import Rotation as R # quaternion in scalar-last
+from skimage.draw import circle_perimeter
+
+f = np.float64(1.9/5.6)
 
 def main():
     # world coordinate frame, origin at ground level of world-plane, contact point of front-wheel and road
@@ -40,14 +43,69 @@ def main():
                           parent=world_frame,
                           children=[],
                           transToParent=transform(np.array([0, 121.37, 0], dtype=np.float64),
-                          R.from_quat(np.array([np.sin(np.pi/2), 0, 0, np.cos(np.pi/2)], dtype=np.float64)))
+                          R.from_quat(np.array([0, np.sin(np.pi/2), 0, np.cos(np.pi/2)], dtype=np.float64)))
                           )
 
+    image_frame = cordFrame(data=None,
+                            name='Image',
+                            world_frame=world_frame,
+                            parent=cam_frame,
+                            children=[],
+                            transToParent=transform(np.array([0, 0, 0], dtype=np.float64),
+                            R.from_quat(np.array([0, 0, np.sin(np.pi/2), np.cos(np.pi/2)], dtype=np.float64)))
 
-    print(world_frame)
-    print(plane_frame)
-    print(imu_frame)
-    print(cam_frame)
+    )
+    # print(world_frame)
+    # print(plane_frame)
+    # print(imu_frame)
+    # print(cam_frame)
+    radius = 200
+    curve2 = circle_perimeter(np.int(radius*10), 0, np.abs(radius))
+    curve3d = curve2 + (np.zeros(curve2[0].shape, dtype=np.int64), np.ones(curve2[0].shape, dtype=np.int64))
+    curve3d = np.column_stack(curve3d)
+
+    curve_homog = np.zeros((curve3d.shape[0], curve3d.shape[1]))
+    cam_mat = cam_frame.transToWorld.rotQuat.as_matrix()
+    cam_trans = cam_frame.transToWorld.trans
+
+    homog_rot = np.zeros((cam_mat.shape[0]+1, cam_mat.shape[1]+1))
+    # print(homog_rot)
+    homog_rot[:-1,:-1] = cam_mat
+    homog_rot[-1,-1] = 1
+
+    homog_pos = np.identity(4)
+    homog_pos[:-1, -1] =- cam_trans
+
+    curve_proj = np.zeros_like(curve2)
+
+    for i in range(curve3d.shape[0]):
+        # print(homog_rot)
+        # print(homog_pos)
+        trans_rot = np.dot(homog_rot, homog_pos)
+        curve_homog[i] = np.dot(trans_rot, curve3d[i])
+
+        x_film = f * (curve_homog[i,0]/curve_homog[i,2])
+        y_film = f * (curve_homog[i,1]/curve_homog[i,2])
+
+
+    print(curve_homog)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=='__main__':
     main()
