7ac2b7f36d88ddc975f795d5a7f3210e5391eae8
larsberend <berend.brandt@gmail.com>
2020-06-09 12:03:55 +0200

    faster my_warp2

--- birdview.old.py
+++ birdview.old.py
@@ -10,6 +10,7 @@ from skimage.draw import line
 IMAGE_H = 562
 IMAGE_W = 1920
 
+font = cv.FONT_HERSHEY_SIMPLEX
 # matrices for camera intrinsics
 pixel_mat = np.array([[1/pu,0,u0], [0,1/pv,v0],[0,0,1]], dtype=np.float64)
 focal_mat = np.array([[f,0,0],[0,f,0], [0,0,1]], dtype=np.float64)
@@ -97,21 +98,28 @@ def birdview():
     # quit()
     # for x in np.arange(0, np.pi, 0.000001):
     cnt = 0
-    for x in np.arange(0, np.pi, 0.0001):
-        H = get_homography(rot = R.from_euler('xyz', (x, 0, 0)).as_matrix(),
+    for p in np.arange(0.000, np.pi, 0.1):
+        print(p)
+        H = get_homography(rot = R.from_euler('xyz', (0, p, 0), degrees=False).as_matrix(),
                        t = np.array([0, 0, 0]),
-                       n = np.array([0, 0, 1])
+                       n = np.array([0, 1, 0])
                        )
+        # print(H)
         # large_im = np.zeros((10000, 10000, 3))
         # large_im[4000:5080, 4000:5920] = img
         # warped_img = cv.warpPerspective(large_im, H, (10000, 10000)) # Image warping
-        # small_img = cv.resize(img, (np.int32(img.shape[1]/2), np.int32(img.shape[0]/2)))
-        warped_img = my_warp2(img, H)
+        small_img = cv.resize(img, (np.int32(img.shape[1]/2), np.int32(img.shape[0]/2)))
+        # warped_img = my_warp_vec(small_img, H)
+        warped_img = my_warp3(small_img, H)
+        warped_img2 = my_warp2(small_img, H)
+        # warped_img = cv.rotate(warped_img, cv.ROTATE_90_COUNTERCLOCKWISE)
         # warped_img = cv.warpPerspective(img, H, (1920, 1080), (cv.INTER_LINEAR, cv.WARP_INVERSE_MAP)) # Image warping
         # warped_img = cv.resize(warped_img, (1920, 1080))
-        cv.imshow('warp', warped_img)
-        cv.imwrite('./my_warp/pi2%s.png'%(cnt), warped_img)
-
+        # cv.imshow('warp', warped_img)
+        # cv.putText(warped_img, 'x = %s'%(x), (50,20), font, 0.5, (0, 255, 0), 20, cv.LINE_AA)
+        cv.imwrite('./my_warp/%s.png'%(cnt), warped_img)
+        cv.imwrite('./my_warp/%s-2.png'%(cnt), warped_img2)
+        '''
         # left = np.where(np.all(warped_img == [0,0,255], axis=-1))
         middle = np.where(np.all(warped_img == [0,255,0], axis=-1))
         right = np.where(np.all(warped_img == [255,0,0], axis=-1))
@@ -131,8 +139,7 @@ def birdview():
             # print((slope_l,slope_m,slope_r))
 
         print((slope_r, slope_m))
-
-        quit()
+        '''
         cnt += 1
         if cv.waitKey(1) & 0xFF == ord('q'):
             print(H)
@@ -145,7 +152,7 @@ def birdview():
     # print(homo.T)
 
     quit()
-
+'''
 def my_warp(src, H):
     height, width = src.shape[:2]
     dst_points = np.zeros_like(src)
@@ -157,25 +164,125 @@ def my_warp(src, H):
             a = (H[0,0]*x + H[0,1]*y + H[0,2])/(H[2,0]*x + H[2,1]*y + H[2,2])
             b = (H[1,0]*x + H[1,1]*y + H[1,2])/(H[2,0]*x + H[2,1]*y + H[2,2])
             if a < height and a > 0 and b < width and b > 0:
-                dst_points[x,y] = src[np.int32(a),np.int32(b)]
+                dst_points[x,y] = src[np.int64(a),np.int64(b)]
     return dst_points
-
+'''
 def my_warp2(src, H):
-    height, width = 3000, 3000
+    height, width = 1000, 1000
     # height, width = src.shape[:2]
-    dst_points = np.zeros((height, width, 3))
+    dst_points = np.zeros((height, width, 3), dtype=np.float64)
     # print(H)
     # print((H[0,2], H[1,2], H[2,2]))
     # quit()
+    cnt_tru=0
     for x in np.arange(-height/2, height/2):
         for y in np.arange(-width/2, width/2):
-            a = (H[0,0]*x + H[0,1]*y + H[0,2])/(H[2,0]*x + H[2,1]*y + H[2,2]) + src.shape[0]/2
-            b = (H[1,0]*x + H[1,1]*y + H[1,2])/(H[2,0]*x + H[2,1]*y + H[2,2]) + src.shape[1]/2
+            a = np.float64((H[0,0]*x + H[0,1]*y + H[0,2])/(H[2,0]*x + H[2,1]*y + H[2,2]) + src.shape[0]/2)
+            b = np.float64((H[1,0]*x + H[1,1]*y + H[1,2])/(H[2,0]*x + H[2,1]*y + H[2,2]) + src.shape[1]/2)
 
             # if a < height and a > 0 and b < width and b > 0:
             if a < src.shape[0] and a > 0 and b < src.shape[1] and b > 0:
             # if np.abs(a) < height/2 and np.abs(b) < width/2:
-                dst_points[np.int32(x+height/2),np.int32(y+width/2)] = src[np.int32(a),np.int32(b)]
+                cnt_tru+=1
+                dst_points[np.int64(x+height/2),np.int64(y+width/2)] = src[np.int64(a),np.int64(b)]
+                dst_points[10,10] = [255,255,0]
+        # print(y)
+    return dst_points
+
+def my_warp3(src, H):
+    # height, width = 1000, 1000
+    # width, height = 1000, 10000
+    height, width = src.shape[:2]
+    dst_points = np.zeros((height, width, 3), dtype=np.float64)
+    # print(H)
+    # print((H[0,2], H[1,2], H[2,2]))
+    # quit()
+    cnt_tru=0
+    a_vec = np.zeros((height, width))
+    b_vec = np.zeros((height, width))
+    # for x in np.arange(height):
+    #     for y in np.arange(width):
+
+
+
+    for x in np.arange(-height/2, height/2):
+        for y in np.arange(-width/2, width/2):
+            a = np.float64((H[0,0]*x + H[0,1]*y + H[0,2]) / (H[2,0]*x + H[2,1]*y + H[2,2])+ src.shape[0]/2)
+            b = np.float64((H[1,0]*x + H[1,1]*y + H[1,2]) / (H[2,0]*x + H[2,1]*y + H[2,2])+ src.shape[1]/2)
+            # a_vec[np.int64(x), np.int64(y)] = a
+            # b_vec[np.int64(x), np.int64(y)] = b
+            a_vec[np.int64(x+height/2), np.int64(y+width/2)] = a
+            b_vec[np.int64(x+height/2), np.int64(y+width/2)] = b
+
+
+    # a_vec[a_vec<0] = 0
+    # b_vec[b_vec<0] = 0
+    #
+    # a_vec[a_vec>height] = 0
+    # b_vec[b_vec>width] = 0
+    #
+    # print(np.amin(a_vec))
+    # print(np.amax(a_vec))
+    # print(np.amin(b_vec))
+    # print(np.amax(b_vec))
+
+    a_vec = np.float32(a_vec)
+    b_vec = np.float32(b_vec)
+    # src[0,0] = 255,255,0
+    # src[:,0] = 255,255,0
+    # src[0,:] = 255,255,0
+    dst_points = cv.remap(src, b_vec, a_vec, 0)
+                #
+                # cnt_tru+=1
+                # dst_points[np.int64(x+height/2),np.int64(y+width/2)] = src[np.int64(a),np.int64(b)]
+                # dst_points[10,10] = [255,255,0]
+        # print(y)
+    return dst_points
+
+
+
+
+def my_warp_vec(src, H):
+    height, width = 1000, 1000
+    dst_points = np.zeros((height, width, 3), dtype=np.float64)
+
+    x_vec = np.arange(-height/2, height/2)
+    y_vec = np.arange(-width/2, width/2)
+
+    a_vec = np.int64((H[0,0]*x_vec + H[0,1]*y_vec + H[0,2])/(H[2,0]*x_vec + H[2,1]*y_vec + H[2,2]) + src.shape[0]/2)
+    b_vec = np.int64((H[1,0]*x_vec + H[1,1]*y_vec + H[1,2])/(H[2,0]*x_vec + H[2,1]*y_vec + H[2,2]) + src.shape[1]/2)
+
+    print((a_vec.shape, b_vec.shape))
+    cnt_tru = 0
+    for x in np.int64(x_vec+height/2):
+        for y in np.int64(y_vec+width/2):
+            if a_vec[x] < src.shape[0] and a_vec[x] > 0 and b_vec[y] < src.shape[1] and b_vec[y] > 0:
+                cnt_tru+=1
+                dst_points[np.int64(x),np.int64(y)] = src[a_vec[x],b_vec[y]]
+    print(cnt_tru)
+    return dst_points
+def qsda():
+    print((a_vec.shape, b_vec.shape))
+
+    truth_a = a_vec < src.shape[0]
+    truth_a = np.logical_and(truth_a, a_vec > 0)
+    truth_b = b_vec > 0
+    truth_b = np.logical_and(truth_b, b_vec < src.shape[1])
+    # print(truth.dtype)
+
+    # a_vec = a_vec[a_vec<src.shape[0]]
+    # a_vec = a_vec[a_vec>0]
+    # b_vec = b_vec[b_vec<src.shape[1]]
+    # b_vec = b_vec[b_vec>0]
+
+
+
+    print((a_vec.shape, b_vec.shape))
+    # print(dst_points[np.int64(x_vec[truth_a]+height/2), np.int64(y_vec[truth_b]+width/2)].shape)
+    print(src[a_vec[truth_a],b_vec[truth_b]].shape)
+    dst_points[np.int64(x_vec[truth_a]+height/2), np.int64(y_vec[truth_b]+width/2)] = src[a_vec[truth_a],b_vec[truth_b]]
+    # dst_points[x_vec+height/2, y_vec+width/2] = src[a_vec,b_vec]
+
     return dst_points
 
 def my_warp_lanes(srcY, srcX, width, height, H):
@@ -205,9 +312,22 @@ def get_homography(rot, t, n):
     # rot = R.from_euler('xyz', (0, 0, np.pi/4)).as_matrix()
     # print(rot)
     # print(np.dot(t, n.T))
+    # print(rot)
+    # print(R.from_matrix(rot).as_euler('xyz', degrees=False))
+    print('rot')
+    print(rot)
+
+
+    n = n.reshape((3,1))
+    t = t.reshape((3,1))
+
+    print('tn')
+    print(np.dot(t, n.T))
+
     H = rot + np.dot(t, n.T)
     H = H / H[2,2]
-
+    print('H')
+    print(H)
     return H
 
 
