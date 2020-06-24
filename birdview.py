4cd7fe8afffeec58d7d7fc58772775c32a48d19a
larsberend <berend.brandt@gmail.com>
2020-06-24 14:22:05 +0200

    road marking detection with hough lines

--- birdview.py
+++ birdview.py
@@ -7,6 +7,9 @@ from scipy.stats import linregress
 from scipy.ndimage import maximum_filter
 from draw_curve import pu,pv,u0,v0,f
 from skimage.draw import line
+import skimage.transform
+from sklearn.cluster import KMeans
+from sklearn import linear_model
 
 IMAGE_H = 562
 IMAGE_W = 1920
@@ -14,79 +17,119 @@ IMAGE_W = 1920
 font = cv.FONT_HERSHEY_SIMPLEX
 
 
-def birdview(img, view):
+def birdview(img, view, last_angle):
     focal_mat = np.array([[f,0,0],[0,f,0], [0,0,1]], dtype=np.float64)
     pixel_mat = np.array([[1/pu,0,u0], [0,1/pv,v0],[0,0,1]], dtype=np.float64)
     K = np.dot(pixel_mat, focal_mat)
+    warped_img = None
+
+    yellow = np.where(np.all(img == [0,204,204], axis=-1))
+    blue = np.where(np.all(img == [204,204,0], axis=-1))
+    # left = np.where(np.all(warped_img == [0,0,255], axis=-1))
+    # print(yellow)
+    # for x,y in zip(yellow[0], yellow[1]):
+    #     img = cv.circle(img, (y,x), 10,  (0,0,255))
+    # cv.imwrite('cirles.png', img)
+    new_height = np.amin([min(yellow[0]),min(blue[0])])
+    new_width = np.amax([max(yellow[1]),max(blue[1])])
+
+    img = img[new_height:, 0:new_width+1]
+
+    # yellow = np.where(np.all(img == [0,204,204], axis=-1))
+    # blue = np.where(np.all(img == [204,204,0], axis=-1))
+
+    if last_angle is not None:
+        search_grid = np.arange(last_angle-0.1, last_angle+0.1, 0.001)
+    else:
+        search_grid = np.arange(0, np.pi, 0.01)
 
     cnt = 0
-    for p in np.arange(0.1, np.pi, 0.1):
+    # print(search_grid.shape)
+    for p in search_grid:
         # print(p)
         rot = R.from_euler('xyz', (0, p, 0), degrees=False).as_matrix()
-
         H = get_homography2(rot, K)
-        # print(H)
-
-        # small_img = cv.resize(img, (np.int32(img.shape[1]/3), np.int32(img.shape[0]/3)))
-
-        middle = np.where(np.all(img == [0,255,0], axis=-1))
-        right = np.where(np.all(img == [255,0,0], axis=-1))
-        # left = np.where(np.all(warped_img == [0,0,255], axis=-1))
-
-        new_height = np.amin([min(middle[0]),min(right[0])])
-        new_width = np.amax([max(middle[1]),max(right[1])])
-
 
         if view:
-            img = img[new_height:, 0:new_width+1]
             warped_img = warp_img(img, H)
-            middle_warp = np.where(np.all(warped_img == [0,255,0], axis=-1))
-            right_warp = np.where(np.all(warped_img == [255,0,0], axis=-1))
+            yellow_warp = np.where(np.all(warped_img == [0,204,204], axis=-1))
+            blue_warp = np.where(np.all(warped_img == [204,204,0], axis=-1))
             # pass x as y values and vice versa to avoid infinite slope
-            slope_m, intercept_m = linregress(middle_warp[0], middle_warp[1])[:2]
-            slope_r, intercept_r = linregress(right_warp[0], right_warp[1])[:2]
-            warped_img = cv.line(warped_img, pt1=(np.int32(intercept_m), 0), pt2=(np.int32(slope_m*warped_img.shape[0] + intercept_m), warped_img.shape[0]), color=(0,255,0))
-            warped_img = cv.line(warped_img, pt1=(np.int32(intercept_r), 0), pt2=(np.int32(slope_r*warped_img.shape[0] + intercept_r), warped_img.shape[0]), color=(255,0,0))
 
-            # cv.imwrite('./my_warp/%s-2.png'%(cnt), warped_img)
+            ransac_y = linear_model.RANSACRegressor()
+
+            ransac_y.fit(yellow_warp[0].reshape(len(yellow_warp[0]), 1), yellow_warp[1])
+            line_ransac_y = ransac_y.predict(yellow_warp[0].reshape(len(yellow_warp[0]), 1))
+
+            slope_y, intercept_y = linregress(yellow_warp[0], line_ransac_y)[:2]
+            # print(slope_y)
+
+            ransac_b = linear_model.RANSACRegressor()
+            ransac_b.fit(blue_warp[0].reshape(len(blue_warp[0]), 1), blue_warp[1])
+            line_ransac_b = ransac_b.predict(blue_warp[0].reshape(len(blue_warp[0]), 1))
+
+            slope_b, intercept_b = linregress(blue_warp[0], line_ransac_b)[:2]
+            # print(slope_b)
+
+            warped_img = cv.line(warped_img, pt1=(np.int32(intercept_y), 0), pt2=(np.int32(slope_y*warped_img.shape[0] + intercept_y), warped_img.shape[0]), color=(0,255,0))
+            warped_img = cv.line(warped_img, pt1=(np.int32(intercept_b), 0), pt2=(np.int32(slope_b*warped_img.shape[0] + intercept_b), warped_img.shape[0]), color=(255,0,0))
+
+            cv.imwrite('./my_warp/%s-3.png'%(cnt), warped_img)
 
 
 
 
         else:
-            middle = np.where(np.all(img == [0,255,0], axis=-1))
-            right = np.where(np.all(img == [255,0,0], axis=-1))
-            # print(middle)
-            # print(right)
-            middle_warp = point_warp(middle, H, img)
-            right_warp = point_warp(right, H, img)
-            # print(middle_warp)
-            # print(right_warp)
+            # print(yellow)
+            # print(blue)
+            yellow_warp = point_warp(yellow, H, img)
+            blue_warp = point_warp(blue, H, img)
+            # print(yellow_warp)
+            # print(blue_warp)
 
             # slope_l = linregress(left[0], left[1])[0]
             # pass x as y values and vice versa to avoid infinite slope
-            slope_m, intercept_m = linregress(middle_warp[0], middle_warp[1])[:2]
-            slope_r, intercept_r = linregress(right_warp[0], right_warp[1])[:2]
+            # slope_y, intercept_y = linregress(yellow_warp[0], yellow_warp[1])[:2]
+            # slope_b, intercept_b = linregress(blue_warp[0], blue_warp[1])[:2]
+            ransac_y = linear_model.RANSACRegressor()
+
+            ransac_y.fit(yellow_warp[0].reshape(len(yellow_warp[0]), 1), yellow_warp[1])
+            line_ransac_y = ransac_y.predict(yellow_warp[0].reshape(len(yellow_warp[0]), 1))
+
+            slope_y, intercept_y = linregress(yellow_warp[0], line_ransac_y)[:2]
+            # print(slope_y)
+
+            ransac_b = linear_model.RANSACRegressor()
+            ransac_b.fit(blue_warp[0].reshape(len(blue_warp[0]), 1), blue_warp[1])
+            line_ransac_b = ransac_b.predict(blue_warp[0].reshape(len(blue_warp[0]), 1))
+
+            slope_b, intercept_b = linregress(blue_warp[0], line_ransac_b)[:2]
+            # print(slope_b)
+            # warped_img = cv.line(warped_img, pt1=(np.int32(intercept_y), 0), pt2=(np.int32(slope_y*warped_img.shape[0] + intercept_y), warped_img.shape[0]), color=(0,255,0))
+            # cv.imwrite('./guckstehier.png', warped_img)
+            # quit()
 
             # print(((np.int32(intercept_m), 0), (np.int32(slope_m*warped_img.shape[0] + intercept_m), warped_img.shape[0])))
             # print(((np.int32(intercept_r), 0), (np.int32(slope_r*warped_img.shape[0] + intercept_r), warped_img.shape[0])))
 
-        print((slope_r, slope_m))
+        # print((slope_y, slope_b))
 
 
-        tol = 1e-03
-        if np.isclose(slope_r, slope_m, tol, equal_nan=False):
-            print('ja nice!')
-            print((slope_r,slope_m))
-            warped_img = warp_img(img,H)
+        tol = 1e-02
+        if np.isclose(slope_y, slope_b, tol, equal_nan=False):
+            # print('ja nice!')
+            # print((slope_y,slope_b))
+            # warped_img = warp_img(img,H)
+            #
+            # # print(np.unique(warped_img, return_counts=True))
+            # cv.imwrite('./parallel.png', warped_img)
 
-            # print(np.unique(warped_img, return_counts=True))
-            cv.imwrite('./parallel.png', warped_img)
-            return p
+            return warped_img, p
 
         cnt += 1
+
     print('No fitting angle found.')
-    return None
+    return warped_img, last_angle
 
 def point_warp(points, H, img):
     # X = np.array([0, points[0], img.shape[0]])
@@ -183,12 +226,12 @@ def warp_img(src, H):
 
 # from Gerhard Roth
 def get_homography2(rot, K):
-    print(rot)
+    # print(rot)
     KR = np.dot(K, rot)
-    print(KR)
+    # print(KR)
     KRK = np.dot(KR, np.linalg.inv(K))
     # print(KRK)
-    print('end of homography2')
+    # print('end of homography2')
 
     return np.linalg.inv(KRK)
 
@@ -201,13 +244,159 @@ def get_homography(rot, t, n):
     H = H / H[2,2]
     return H
 
-def mark_lanes(img):
+# from https://medium.com/@galen.ballew/opencv-lanedetection-419361364fc0
+def mark_lanes(img, roll_angle):
+    # img = rotate_image(img, roll_angle)
+    roll_angle = np.degrees(roll_angle)
+    img = skimage.transform.rotate(img, -roll_angle, clip=True, preserve_range=True).astype(np.uint8)
+    # print(img.dtype)
+
+    cv.imwrite('schaunwirmal.png', img)
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    kernel_size = 5
+    gauss_gray = cv.GaussianBlur(gray, (kernel_size,kernel_size), 0)
+    cv.imwrite('schaunwirmal1.png', gauss_gray)
+    mask_white = cv.inRange(gauss_gray, 230, 255)
+    mask_w_image = cv.bitwise_and(gauss_gray, mask_white)
+    # cv.imwrite('schaunwirmal2.png', gauss_gray)
+    # kernel_size = 5
+    # gauss_gray = cv.GaussianBlur(mask_w_image, (kernel_size,kernel_size), 0)
+    cv.imwrite('schaunwirmal2.png', mask_w_image)
+
+    low_threshold = 50
+    high_threshold = 150
+    canny_edges = cv.Canny(mask_w_image,low_threshold,high_threshold)
+
+    imshape = img.shape
+    lower_left = [0,imshape[0]]
+    # lower_left = [imshape[1]/9,imshape[0]]
+    lower_right = [imshape[1],imshape[0]]
+
+    if roll_angle < 0:
+        top_left = [imshape[1]/4, 2*imshape[0]/3]
+        top_right = [5*imshape[1]/6, 2*imshape[0]/3]
+    else:
+        top_left = [imshape[1]/3, 2*imshape[0]/3]
+        top_right = [3*imshape[1]/6, 2*imshape[0]/3]
+
+    vertices = [np.array([lower_left,top_left,top_right,lower_right],dtype=np.int32)]
+    # print(vertices)
+    roi_image = region_of_interest(canny_edges, vertices)
+    # print(np.array_equal(roi_image, canny_edges))
+    cv.imwrite('schaunwirmal3.png', roi_image)
+
+    #rho and theta are the distance and angular resolution of the grid in Hough space
+    #same values as quiz
+    rho = 2
+    theta = np.pi/180
+    #threshold is minimum number of intersections in a grid for candidate line to go to output
+    threshold = 150
+    min_line_len = 50
+    max_line_gap = 70
+
+    lines = cv.HoughLinesP(roi_image, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    if lines is not None:
+
+        # print(lines)
+        klines = lines.reshape((lines.shape[0], lines.shape[-1]))
+        # print(klines)
+        # print(lines)
+
+        if lines.shape[0] > 1:
+            kmeans2 = KMeans(n_clusters = 2)
+            # kmeans3 = KMeans(n_clusters = 3)
 
 
 
+            klines2 = kmeans2.fit_predict(klines)
+            # klines3 = kmeans3.fit_predict(klines)
+            # print(kmeans2.inertia_)
+            # print(lines)
 
+            # klines3 = kmeans3.fit_predict(lines)
+            # print(kmeans2.get_params())
+            #
+            # if kmeans3.inertia_ < kmeans2.inertia_:
+            #     klines = klines3
+            #     print('three lines')
+            # else:
+            #     klines = klines2
+            #     print('two lines')
+            #
+            # print(lines)
+            # print(klines)
+            # quit()
+
+            klines = klines2
+
+            c1, c2 = kmeans2.cluster_centers_.astype(np.int32)
+            cv.line(img, (c1[0], c1[1]), (c1[2], c1[3]), (120,120,0), 50)
+            cv.line(img, (c2[0], c2[1]), (c2[2], c2[3]), (0,120,120), 50)
+
+            line_img = np.zeros((roi_image.shape[0], roi_image.shape[1], 3), dtype=np.uint8)
+            for l in range(lines.shape[0]):
+                for x1,y1,x2,y2 in lines[l]:
+                    if klines[l]==0:
+                        cv.line(img, (x1, y1), (x2, y2), (255,255,0), 2)
+                    # elif klines[l]==1:
+                    #     cv.line(img, (x1, y1), (x2, y2), (0,255,0), 2)
+                    else:
+                        cv.line(img, (x1, y1), (x2, y2), (0,255,255), 2)
+            weighted_img = cv.addWeighted(img, 0.8, line_img, 1, 0)
+
+            # cv.imwrite('schaunwirmal4.png', weighted_img)
+
+            for v in vertices:
+                for x,y in v:
+                    cv.circle(weighted_img, (x,y), 5,  (255,0,0), -1)
+            # weighted_img = skimage.transform.rotate(weighted_img, roll_angle/2, clip=True, preserve_range=True)
+            # weighted_img = rotate_image(weighted_img, -roll_angle/2)
+            cv.imwrite('schaunwirmal5.png', weighted_img)
+            return weighted_img, True
+    return img, False
+
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    #filling pixels inside the polygon defined by "vertices" with the fill color
+    mask = cv.fillPoly(mask, vertices, ignore_mask_color)
+
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv.bitwise_and(img, mask)
+    return masked_image
+
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+  return result
 
 if __name__ == '__main__':
-    img = cv.imread('./00-08-52-932_points.png') # Read the test img
-    mark_lanes(img)
-    # birdview(img, False)
+    # img = cv.imread('./00-08-52-932_points.png') # Read the test img
+    # img = cv.imread('./3_00-01-48.png') # Read the test img
+    # img, roll_angle= cv.imread('./problematic.png'), 0.298919415637517
+    # img, roll_angle= cv.imread('./problematic2.png'), 0.171368206765908
+    img, roll_angle= cv.imread('./problematic3.png'), -0.340036930698958
+
+    # rotate image by specified roll angle
+
+
+    marked_img, retval = mark_lanes(img, -roll_angle*2)
+    # print(retval)
+    # warped_img, angle = birdview(marked_img, False, None)
+    # cv.imwrite('aha.png', warped_img)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from birdview import mark_lanes, birdview
 from cordFrames import cordFrame, worldFrame, transform, get_cordFrames
 from scipy.spatial.transform import Rotation as R # quaternion in scalar-last
 from skimage.draw import circle_perimeter, line_aa, bezier_curve
@@ -14,12 +15,13 @@ Updates camera-parameters with data from IMU.
 '''
 
 def main():
-
+    bv = True
+    angle_calc = None
     world_frame, fw_frame, cam_frame, imu_frame = get_cordFrames()
 
     csv_path = '../100GOPRO/testfahrt_1006/kandidaten/csv/'
     video_path = '../100GOPRO/testfahrt_1006/kandidaten/'
-    file = '2_2'
+    file = '3_2'
     start_msec = 000.0
     font = cv.FONT_HERSHEY_SIMPLEX
 
@@ -39,106 +41,139 @@ def main():
         # capture frame-by-frame
         ret, frame = cap.read()
         if ret:
+
+
+
+
+
             # print('ret')
             height,width = frame.shape[:2]
             # find closest Datapoint in time to current frame
             nearest_rad = find_nearest(radius_madgwick[0], cap.get(0))
             mil, radius, ori = radius_madgwick[:,nearest_rad]
             # cast Quat from string(pandas) to float
+
+
+
+
+
+
             if type(ori)==str:
                 ori = [np.float64(x.strip(' []')) for x in ori.split(',')]
             # rearrange to scalar-last format, cast to Euler
             ori = [ori[1],ori[2],ori[3],ori[0]]
             ori_eul = R.from_quat(ori).as_euler('xyz',degrees=False)
-            # check if Euler conversion returns same Quaternion:
-            # assert np.abs(np.dot(R.from_euler('xyz', ori_eul).as_quat(), ori) - 1) < 0.00001
-
-            # update fw_frame with pitch & yaw
-            # ori_xy_fw = R.from_euler('xyz', [ori_eul[0], ori_eul[1], 0])
-            ori_x_fw = R.from_euler('xyz', [ori_eul[0], 0, 0])
-            # fw_trans = transform([0,0,0], ori_xy_fw.as_quat())
-            fw_trans = transform([0,0,0], ori_x_fw.as_quat())
-            fw_frame.update_ori(fw_trans)
-
-            ori_z_imu = R.from_euler('xyz',[0, 0, -ori_eul[2]], degrees=False)
-            ori_yz_imu = R.from_euler('xyz',[0, -ori_eul[1], -ori_eul[2]], degrees=False)
-
-            # ckeck translation of camera with angle of 0 and 90
-            # ori_z_imu = R.from_euler('xyz',[0, 0, -np.pi/2],degrees=False)
-            # ori_z_imu = R.from_euler('xyz',[0, 0, 0],degrees=False)
-
-            # pos_from_angle = ori_yz_imu.apply([0, 1.1237, 0])
-            pos_from_angle = ori_yz_imu.apply([0, 1.1237, 0])
-            # print(pos_from_angle)
-
-            # calc new transform of IMU to world
-            imu_trans = transform(pos_from_angle, [
-                                                    [0, 0, np.sin(np.pi/4),np.cos(np.pi/4)],
-                                                    [np.sin(np.pi/2), 0, 0, np.cos(np.pi/2)],
-                                                    # [ori_z_imu[1], 0, 0, np.cos(np.arcsin(ori_z_imu[1]))]
-                                                    # ori_z_imu.as_quat()
-                                                    ori_yz_imu.as_quat()
-                                                    # [0,np.sin(np.pi/4),0,np.cos(np.pi/4)]
-                                                   ]
-                                 )
-            imu_frame.update_ori(imu_trans)
-            # print(imu_frame)
-
-            cv.putText(frame, 'Frame Nr: %s'%(cap.get(0)), (1650,20), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
-            cv.putText(frame, 'Radius: %s'%(radius), (1650,40), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
-            cv.putText(frame, 'MIlliseconds(IMU): %s'%(mil), (1650,60), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
-
-            # calc visible trajectory in frame and view from above
-            curve_proj, bird_view = draw_curve(radius, cam_frame, fw_frame)
-            # print(type(curve_proj))
-            curve_proj=curve_proj[curve_proj[:,0]>=0].astype(np.int32)
-            # curve_proj=curve_proj[curve_proj[:,0]<1920]
-            # curve_proj=curve_proj[curve_proj[:,1]<1080]
-
-            # rr,cc = bezier_curve(*curve_proj[0], *curve_proj[int(curve_proj.shape[0]/2)], *curve_proj[-1], weight=5)
-            # print((rr,cc))
-            # frame[cc,rr] = 255,255,255
 
 
-            # curve_proj=curve_proj[np.abs(curve_proj[:,0]-960).argsort()]
-            # print(curve_proj)
-            # for l in range(curve_proj.shape[0]-1):
-            #     # print(curve_proj[l])
-            #     rr,cc,val = line_aa(*curve_proj[l], *curve_proj[l+1])
-            #     # print((rr.shape,cc.shape))
-            #     vals = np.full((val.shape[0],3),fill_value = 255)
-            #     vals[:,1] = val*255
-            #
-            #     # print(frame[0,1])
-            #     # print(val.shape)
-            #     frame[cc,rr] = vals
-            #     # quit()
+
+            if bv == True:
+                new_frame, retval = mark_lanes(frame, -ori_eul[2]*2)
+                if retval:
+                    img, angle_calc = birdview(new_frame, False, angle_calc)
+                    if angle_calc is not None:
+                        # print(angle_calc)
+                        print(np.degrees(angle_calc))
+                else:
+                    print('no lines found in image')
+                cv.imwrite('../100GOPRO/testfahrt_1006/kandidaten/%s_marked/%s.png'%(file, frame_nr_int), new_frame)
+                cv.imshow(file, new_frame)
+                print(frame_nr_int)
 
 
-            # frame2 = np.zeros_like(frame)
-            frame2 = frame.copy()
-            a = curve_proj.astype(np.int32),curve_proj[1:].astype(np.int32)
-            # print(curve_proj.shape[0])
-            for j in range(curve_proj.shape[0]):
-                # b,g,r = frame[curve_proj[j][0],curve_proj[j][1]]
-                cv.circle(frame, (curve_proj[j,0], curve_proj[j,1]), 5, (0,255,0))
-                # print(int(255/(j+1)))
-                # cv.line(frame, tuple(a[0][j]), tuple(a[1][j]), (int(b), int(g), int(255/(j+1))), 10, lineType=cv.LINE_8)
-            # frame2[frame2==0] = frame[frame2==0]
-            # frame = cv.addWeighted(frame, 0.5,frame2, 0.5, 0)
 
-            # frame = ((frame+frame2)/2).astype(np.uint8)
-            # frame[frame>255]=255
-            cv.circle(frame, (956,559), 10, (255,0,0))
+            else:
+                # check if Euler conversion returns same Quaternion:
+                # assert np.abs(np.dot(R.from_euler('xyz', ori_eul).as_quat(), ori) - 1) < 0.00001
 
-            x_offset = width - bird_view.shape[1]
-            y_offset = np.int(height/3) - bird_view.shape[0]
-            frame[y_offset:y_offset+bird_view.shape[0], x_offset:x_offset+bird_view.shape[1]] = bird_view
+                # update fw_frame with pitch & yaw
+                # ori_xy_fw = R.from_euler('xyz', [ori_eul[0], ori_eul[1], 0])
+                ori_x_fw = R.from_euler('xyz', [ori_eul[0], 0, 0])
+                # fw_trans = transform([0,0,0], ori_xy_fw.as_quat())
+                fw_trans = transform([0,0,0], ori_x_fw.as_quat())
+                fw_frame.update_ori(fw_trans)
 
-            # save every frame as png
-            cv.imwrite('../100GOPRO/testfahrt_1006/kandidaten/%s_processed/%s.png'%(file, frame_nr_int), frame)
+                ori_z_imu = R.from_euler('xyz',[0, 0, -ori_eul[2]], degrees=False)
+                ori_z_imu_pos = R.from_euler('xyz',[0, 0, -ori_eul[2]], degrees=False)
+                ori_yz_imu = R.from_euler('xyz',[0, ori_eul[1], -ori_eul[2]], degrees=False)
+
+                # ckeck translation of camera with angle of 0 and 90
+                # ori_z_imu = R.from_euler('xyz',[0, 0, -np.pi/2],degrees=False)
+                # ori_z_imu = R.from_euler('xyz',[0, 0, 0],degrees=False)
+
+                pos_from_angle = ori_yz_imu.apply([0, 1.1237, 0])
+                # pos_from_angle = ori_z_imu_pos.apply([0, 1.1237, 0])
+
+                # print(pos_from_angle)
+
+                # calc new transform of IMU to world
+                imu_trans = transform(pos_from_angle, [
+                                                        [0, 0, np.sin(np.pi/4),np.cos(np.pi/4)],
+                                                        [np.sin(np.pi/2), 0, 0, np.cos(np.pi/2)],
+                                                        # [ori_z_imu[1], 0, 0, np.cos(np.arcsin(ori_z_imu[1]))]
+                                                        ori_z_imu.as_quat()
+                                                        # ori_yz_imu.as_quat()
+                                                        # [0,np.sin(np.pi/64),0,np.cos(np.pi/64)]
+                                                       ]
+                                     )
+                imu_frame.update_ori(imu_trans)
+                # print(imu_frame)
+
+                cv.putText(frame, 'Frame Nr: %s'%(cap.get(0)), (1650,20), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
+                cv.putText(frame, 'Radius: %s'%(radius), (1650,40), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
+                cv.putText(frame, 'MIlliseconds(IMU): %s'%(mil), (1650,60), font, 0.5, (0, 255, 0), 2, cv.LINE_AA)
+
+                # calc visible trajectory in frame and view from above
+                curve_proj, top_view = draw_curve(radius, cam_frame, fw_frame)
+                # print(type(curve_proj))
+                curve_proj=curve_proj[curve_proj[:,0]>=0].astype(np.int32)
+                # curve_proj=curve_proj[curve_proj[:,0]<1920]
+                # curve_proj=curve_proj[curve_proj[:,1]<1080]
+
+                # rr,cc = bezier_curve(*curve_proj[0], *curve_proj[int(curve_proj.shape[0]/2)], *curve_proj[-1], weight=5)
+                # print((rr,cc))
+                # frame[cc,rr] = 255,255,255
+
+
+                # curve_proj=curve_proj[np.abs(curve_proj[:,0]-960).argsort()]
+                # print(curve_proj)
+                # for l in range(curve_proj.shape[0]-1):
+                #     # print(curve_proj[l])
+                #     rr,cc,val = line_aa(*curve_proj[l], *curve_proj[l+1])
+                #     # print((rr.shape,cc.shape))
+                #     vals = np.full((val.shape[0],3),fill_value = 255)
+                #     vals[:,1] = val*255
+                #
+                #     # print(frame[0,1])
+                #     # print(val.shape)
+                #     frame[cc,rr] = vals
+                #     # quit()
+
+
+                # frame2 = np.zeros_like(frame)
+                # frame2 = frame.copy()
+                a = curve_proj.astype(np.int32),curve_proj[1:].astype(np.int32)
+                # print(curve_proj.shape[0])
+                for j in range(curve_proj.shape[0]):
+                    # b,g,r = frame[curve_proj[j][0],curve_proj[j][1]]
+                    cv.circle(frame, (curve_proj[j,0], curve_proj[j,1]), 5, (0,255,0))
+                    # print(int(255/(j+1)))
+                    # cv.line(frame, tuple(a[0][j]), tuple(a[1][j]), (int(b), int(g), int(255/(j+1))), 10, lineType=cv.LINE_8)
+                # frame2[frame2==0] = frame[frame2==0]
+                # frame = cv.addWeighted(frame, 0.5,frame2, 0.5, 0)
+
+                # frame = ((frame+frame2)/2).astype(np.uint8)
+                # frame[frame>255]=255
+                cv.circle(frame, (956,559), 10, (255,0,0))
+
+
+                x_offset = width - top_view.shape[1]
+                y_offset = np.int(height/3) - top_view.shape[0]
+                frame[y_offset:y_offset+top_view.shape[0], x_offset:x_offset+top_view.shape[1]] = top_view
+
+                # save every frame as png
+                cv.imwrite('../100GOPRO/testfahrt_1006/kandidaten/%s_processed/%s.png'%(file, frame_nr_int), frame)
+                cv.imshow(file, frame)
             frame_nr_int += 1
-            cv.imshow(file, frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
             print(frame_nr_int)
             break
